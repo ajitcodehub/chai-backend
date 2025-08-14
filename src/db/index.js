f5e13bc1bd79e731cbdd90fc747962e8f55151d5
ajitcodehub <nayakajitkumar743@gmail.com>
2025-08-14 10:11:34 -0400

    small chage for test

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log('✅ MongoDB Connected');
+    console.log('✅ MongoDB Connected !');
   } catch (err) {
     console.error('❌ MongoDB Connection Error:', err.message);
     process.exit(1);
